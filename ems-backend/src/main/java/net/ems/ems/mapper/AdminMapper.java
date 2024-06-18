b20d87ff07767ddc6700a88b2d6f5a845038c193
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-06-18 02:04:39 +0200

    adding the AdminMapper containing the AdminDto

--- ems-backend/src/main/java/net/ems/ems/mapper/AdminMapper.java
+++ ems-backend/src/main/java/net/ems/ems/mapper/AdminMapper.java
@@ -1,4 +1,26 @@
 package net.ems.ems.mapper;
 
+import net.ems.ems.dto.AdminDto;
+import net.ems.ems.entity.Admin;
+
 public class AdminMapper {
+    public static AdminDto mapToAdminDto(Admin admin){
+        return new AdminDto(
+                admin.getId(),
+                admin.getFirstName(),
+                admin.getLastName(),
+                admin.getEmail(),
+                admin.getPassword()
+        );
+    }
+
+    public static Admin mapToAdmin(AdminDto adminDto){
+        return new Admin(
+                adminDto.getId(),
+                adminDto.getAdmin_Name(),
+                adminDto.getAdmin_LastName(),
+                adminDto.getAdmin_Email(),
+                adminDto.getAdmin_Password()
+        );
+    }
 }
