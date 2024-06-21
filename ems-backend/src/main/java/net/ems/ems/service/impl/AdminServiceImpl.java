abd861a06241a560a2cc9332206ca43b70311a56
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-06-21 03:37:02 +0200

    spring boot annotation for allargsconstructor and adding the service

--- ems-backend/src/main/java/net/ems/ems/service/impl/AdminServiceImpl.java
+++ ems-backend/src/main/java/net/ems/ems/service/impl/AdminServiceImpl.java
@@ -1,18 +1,23 @@
 package net.ems.ems.service.impl;
 
+import lombok.AllArgsConstructor;
 import net.ems.ems.dto.AdminDto;
 import net.ems.ems.entity.Admin;
 import net.ems.ems.mapper.AdminMapper;
 import net.ems.ems.repository.AdminRepository;
 import net.ems.ems.service.AdminService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor
 public class AdminServiceImpl implements AdminService {
 
     private final AdminRepository adminRepository;
 
     private final PasswordEncoder passwordEncoder;
 
+    @Override
     public AdminDto createAdmin(AdminDto adminDto){
         Admin admin = AdminMapper.mapToAdmin(adminDto);
         admin.setPassword(passwordEncoder.encode((admin.getPassword())));
