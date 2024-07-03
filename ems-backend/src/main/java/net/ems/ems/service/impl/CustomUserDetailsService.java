0d87f23aa6bd5d4c2c143844681c148e24a10b9d
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-07-03 20:56:22 +0200

    adding the CustomerUserdetails admd adding the roles

--- ems-backend/src/main/java/net/ems/ems/service/impl/CustomUserDetailsService.java
+++ ems-backend/src/main/java/net/ems/ems/service/impl/CustomUserDetailsService.java
@@ -1,4 +1,30 @@
 package net.ems.ems.service.impl;
 
-public class CustomUserDetailsService {
+import lombok.AllArgsConstructor;
+import net.ems.ems.entity.Admin;
+import net.ems.ems.exception.ResourceNotFoundException;
+import net.ems.ems.repository.AdminRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final AdminRepository adminRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+        Admin admin = adminRepository.findByEmail(email)
+                .orElseThrow(()-> new ResourceNotFoundException("Admin not found with email:"+email));
+        return User.builder()
+                .username(admin.getEmail())
+                .password(admin.getPassword())
+                .roles("ADMIN")
+                .build();
+
+    }
 }
