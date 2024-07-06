7586ad85d8352e2d3e882ad7bf9f95132b110e35
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-07-06 20:41:49 +0200

    Admin Controller initialization

--- ems-backend/src/main/java/net/ems/ems/controller/AdminController.java
+++ ems-backend/src/main/java/net/ems/ems/controller/AdminController.java
@@ -1,4 +1,42 @@
 package net.ems.ems.controller;
 
+
+import lombok.AllArgsConstructor;
+import net.ems.ems.dto.AdminDto;
+import net.ems.ems.service.AdminService;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/admins")
+@AllArgsConstructor
+
 public class AdminController {
+
+    private final AdminService adminService;
+
+    @PostMapping
+    public ResponseEntity<AdminDto> createAdmin(@RequestBody AdminDto adminDto){
+        AdminDto createdAdmin = adminService.createAdmin(adminDto);
+        return new ResponseEntity<>(createdAdmin, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<AdminDto> getAdminById(@PathVariable("id") Long adminId) {
+        AdminDto adminDto = adminService.getAdminById(adminId);
+        return ResponseEntity.ok(adminDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AdminDto>> getAllAdmins(){
+        List<AdminDto> admins = adminService.getAllAdmin();
+        return ResponseEntity.ok(admins);
+    }
+
+    @PutMapping("{id}")
 }
